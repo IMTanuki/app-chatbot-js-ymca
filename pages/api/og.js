f751bc0126fd346b440178a7703de545352e8191
Tanuki <imtanuki@gmail.com>
2023-06-06 18:00:41 -0400

    updates - added - og.js (for image rendering in vercel) - modified - og.js to use import instead of require
    outstanding issues
    - globals.css warning

--- pages/api/og.js
+++ pages/api/og.js
@@ -1,27 +1,24 @@
-const { ImageResponse, Text, Image } = require('@vercel/og');
+import { ImageResponse, Text, Image } from '@vercel/og';
 
-module.exports = {
-	config: {
-		api: {
-			bodyParser: false,
-		},
+export const config = {
+	api: {
+		bodyParser: false,
 	},
+};
 
-	handler(req, res) {
-		const title = 'Ex-wife Bot';
-		const description = 'Stay in touch with your ex-wife';
-		const imgSrc = '/bot.png';
+export default function handler(req, res) {
+	const title = 'Ex-wife Bot';
+	const description = 'Stay in touch with yourl ex-wife';
+	const imgSrc = '/bot.png'; // path to your bot.png
 
-		const ogImage = new ImageResponse(
-			new Image({ src: imgSrc, alt: title },
-				new Text({ align: 'center', y: '50%', fontSize: 96, color: 'white', children: title }),
-			),
-			{
-				width: 1200,
-				height: 600,
-			}
-		);
+	const ogImage = new ImageResponse(
+		new Image({ src: imgSrc, alt: title }),
+		new Text({ align: 'center', y: '50%', fontSize: 96, color: 'white', children: title }),
+		{
+			width: 1200,
+			height: 600,
+		},
+	);
 
-		ogImage.render(req, res);
-	}
+	ogImage.render(req, res);
 }
